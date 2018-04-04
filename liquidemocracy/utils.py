949d2d23683eb8ee364a0a4a38a70d2c0e7f6170
Alex <alexrichards92@me.com>
2018-04-04 12:06:13 -0700

    heroku?

--- liquidemocracy/utils.py
+++ liquidemocracy/utils.py
@@ -13,12 +13,19 @@ def send_email(user_email, subject, html):
     msg['From'] = email.utils.formataddr(('Liquidemocracy', 'liquidemocracy.app@gmail.com'))
     msg['Subject'] = subject
 
+    print('\nemail msg: {}\n'.format(msg))
+
     try:
+        print('\nstart\n')
         server = smtplib.SMTP('smtp.gmail.com', 587)
+        print('\nemail server created\n')
         server.starttls()
+        print('\nemail server started\n')
         server.login('liquidemocracy.app@gmail.com', "NxTQj*hTBX7,%|@6")
+        print('\nlogged into email server\n')
         server.sendmail('liquidemocracy.app@gmail.com', user_email, msg.as_string())
-    except Error as e:
+        print('\nsent email\n')
+    except Exception as e:
         print("Email error:", e)
     finally:
         server.quit()
