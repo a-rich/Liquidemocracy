eb266a71c02857948d7b83550a77afcefd7b8ad1
Alex <alexrichards92@me.com>
2018-04-04 10:06:07 -0700

    heroku?

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='liquidemocracy',
     packages=['liquidemocracy'],
     include_package_data=True,
+    python_requires='~=3.5.2',
     install_requires=[
         'flask', 'flask_cors', 'flask_jwt_simple', 'flask-mongoengine',
         'pytest-flask', 'python-dateutil', 'requests', 'beautifulsoup4',
