4ae1ca5dd20912ce4fda7badc8bd30fa88706a91
Alex <alexrichards92@me.com>
2018-04-05 14:48:00 -0700

    testing recommended bills endpoint

--- liquidemocracy/bill_recommender/recommender.py
+++ liquidemocracy/bill_recommender/recommender.py
@@ -166,6 +166,7 @@ def recommend_bills(user_email, filtered_levels):
     interest_vector = [0, 3, 1, 0, 0, 5, 5, 6, 12, 2, 4, 6, 9, 21, 3, 4]
     #interest_vector = list(json.loads(user.interest_vector.to_json()).values())
     interests, non_interests = find_interests(interest_vector)
+    print('before find_interesting_bills() -- filtered_levels is', filtered_levels)
     recommended_bills = find_interesting_bills(interests, user_location, filtered_levels)
 
     return recommended_bills
