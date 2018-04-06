79648db55fb31fb0e37ac0f8b9c0a4b9329cb76a
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-06 14:37:49 -0700

    Small Fix

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
 app.use('/', express.static(__dirname));
-app.listen(process.env.PORT || 5000, '0.0.0.0');
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
