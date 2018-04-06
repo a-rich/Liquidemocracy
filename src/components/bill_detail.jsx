6b6638f7a98160cd2808ff1f0cf6f322ab703b18
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-06 14:22:15 -0700

    heroku stuff

---
+++ src/components/bill_detail.jsx
@@ -0,0 +1,221 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchBill, logoutUser, loginUser } from '../actions';
+import { connect } from 'react-redux';
+import { Field, reduxForm, SubmissionError} from 'redux-form';
+import axios from 'axios';
+
+const ROOT_URL = 'http://liquidemocracy.herokuapp.com';
+
+class BillDetail extends Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {button: ""};
+  }
+
+  	renderField(field) {
+		const { touched , error }  = field.meta;
+		const className = `form-control ${touched && error ? 'border border-danger' : ''}`;
+		let type;
+
+		return (
+				<div className="form-group">
+					<input
+						className={ className }
+						type={field.type}
+						placeholder={field.label}
+						{...field.input}
+					/>
+					<div className="text-help" style={{color: '#B80000'}}>
+					{touched ? error : ''}
+					</div>
+				</div>
+			);
+	}
+
+	componentDidMount() {
+		if(localStorage.getItem("jwt") == null || this.props.user.user.isUserLoggedIn == false) {
+			this.setState({button: "disabled"});
+		}
+		else {
+			this.setState({button: ""});
+		}
+		this.props.fetchBill(this.props.match.params.id);	
+	}
+
+	logout() {
+		this.props.logoutUser();
+		this.props.history.push("/");
+	}
+
+	onSubmit(values) {
+
+	const config = { headers: {
+		'Content-Type': 'application/json'
+	}}
+
+	const request = axios.post(`${ROOT_URL}/api/login/`, values, config)
+	.then( (response) => {
+		if(response.data.error) {
+			throw new error;
+		}
+		else {
+			this.props.loginUser(values);
+			this.props.history.push("/");
+		}
+	})
+	.catch(error => {
+		throw new SubmissionError({_error: 'Failed to login.'});
+	});
+
+	return request;
+	}
+
+	renderBillDefaultDetail() {
+		return (
+			<div>
+				<div className="row no-gutters">
+					<div className="col">
+						<h5>Categories:</h5> {this.props.bill.bill[0].category}
+						<br />
+						<br />
+						<h5>Keywords:</h5>
+					</div>
+					<div className="col-6 text-center card">
+						<h3 className="card-header">{this.props.bill.bill[0].title}</h3>
+						<div className="card-body" style={{'height':'300px', 'overflowY': 'auto'}}>
+							{this.props.bill.bill[0].text}
+							<br />
+						</div>
+						<div className="card-footer text-muted">
+							Vote Date: {new Date(this.props.bill.bill[0].date.$date).toString()}
+						</div>
+					</div>
+					<div className="col">
+						<h4 className="text-center">Vote</h4>
+						<br />
+						<div className="text-center">
+							<b>Voter Count: </b>{this.props.bill.bill[0].vote_info.voter_count}
+						</div>
+						<br />
+						<div className="text-center">
+							<span className="text-success">Yay: {this.props.bill.bill[0].vote_info.yay} </span>
+							| <span className="text-danger">Nay: {this.props.bill.bill[0].vote_info.nay}</span>
+						</div>
+						<br />
+						<div className="text-center">
+							<button className={`btn btn-success col-3 ${this.state.button}`}>Yay</button>
+							<button className={`btn btn-danger col-3 ${this.state.button}`}>Nay</button>
+						</div>
+						<br />
+						<div className="text-center">
+							<button className={`btn btn-info ${this.state.button}`}>Delegate?</button>
+						</div>
+					</div>
+				</div>
+
+				<div className="row no-gutters">
+					<div className="col">
+			
+					</div>
+				</div>
+			</div>
+			)
+	}
+
+	render() {
+		const { handleSubmit, error } = this.props;
+
+		if(!this.props.bill) {
+			return <div>Loading...</div>
+		}
+
+		if(localStorage.getItem("jwt") == null || this.props.user.user.isUserLoggedIn == false) {
+		return (
+				<div className="container-fluid">
+					<nav className="navbar bg-primary">
+					 
+					 <Link className="navbar-brand" style={{color: '#ffffff'}} to="/">Liquidemocracy</Link>
+	                
+	                <div className="login-form" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+		                <form className="form-inline navbar-form" >
+							<Field 
+								label="Email"
+								name="email"
+								type="email"
+								component={this.renderField}
+							/>
+							<Field 
+								label="Password"
+								name="password"
+								type="password"
+								component={this.renderField}
+							/>
+							<button className="btn btn-success" type="submit">Sign in</button>
+							<button className="btn btn-info mx-2" 
+							onClick={() => this.props.history.push("/signup")}>Sign Up</button>
+							<span style={{color: '#B80000'}}>{error}</span>
+						</form>
+					</div>
+					</nav>
+					{this.renderBillDefaultDetail()}
+				</div>
+			)
+		}
+		else
+		{
+			return (
+					<div className="container-fluid">
+						<nav className="navbar bg-primary">
+					 
+					 	<Link className="navbar-brand" style={{color: '#ffffff'}} to="/">Liquidemocracy</Link>
+	                	
+	                	<Link className="nav-item" style={{color: '#ffffff'}} to="/profile">
+						 Profile
+						</Link>
+						<Link className="nav-item active" style={{color: '#ffffff'}} to="/delegates">
+						 Delegates
+						</Link>
+						<Link className="nav-item" style={{color: '#ffffff'}} to="/delegations">
+						 Delegations
+						</Link>
+						<div className="nav-item" 
+						 	 style={{color: '#ffffff', cursor:'pointer'}} 
+						 	 onClick={() => this.logout()}>
+						 Log Out
+						</div>
+						 	
+						</nav>
+						{this.renderBillDefaultDetail()}
+					</div>
+				)
+		}
+	}
+}
+
+function validate(values) {
+
+		const errors = {};
+
+		if(!values.email) {
+			errors.email = "Email blank.";
+		}
+
+		if(!values.password) {
+			errors.password = "Password blank.";
+
+		return errors;
+	}
+}
+
+function mapStateToProps(state) {
+	return {bill: state.bills.bill,
+			user: state.user};
+}
+
+export default reduxForm({
+	validate,
+	form: 'LoginForm'
+})(
+connect(mapStateToProps, {fetchBill, logoutUser, loginUser})(BillDetail)
+);
---
+++ src/components/profile.jsx
@@ -0,0 +1,238 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { logoutUser, fetchProfile } from '../actions';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Field, reduxForm, SubmissionError, initialize } from 'redux-form';
+
+const ROOT_URL = 'http://liquidemocracy.herokuapp.com/api';
+
+class Profile extends Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {editing: false,
+	    			  isEditing: false};
+  }
+
+  renderField(field) {
+		const { touched , error }  = field.meta;
+		const className = `form-control ${touched && error ? 'border border-danger' : ''}`;
+		let type;
+
+		return (
+				<div className="form-group">
+					<label>{field.label}</label>
+					<input
+						className={ className }
+						type={field.type}
+						placeholder={field.label}
+						{...field.input}
+					/>
+					<div className="text-help text-danger">
+					{touched ? error : ''}
+					</div>
+				</div>
+			);
+	}
+
+	onSubmit(values) {
+		let token = localStorage.getItem("jwt");
+
+		const headers = {
+		headers: {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${token}`
+			}
+		}
+
+		axios.post(`${ROOT_URL}/profile/update/`, values, headers)
+		.then(this.setState({editing: false}, () => this.props.fetchProfile()));
+	}
+
+	componentWillMount() {
+		this.props.fetchProfile();
+	}
+
+	shouldComponentUpdate() {
+		if(this.state.isEditing == false)
+		{
+			this.setState({isEditing: true});
+			this.handleInitialize();
+			return true;
+		}
+
+		return true;
+	}
+
+	handleInitialize() {
+		if(this.props.profile.profile != null)
+		{
+			this.props.initialize({email: this.props.profile.profile.email,
+								   name: this.props.profile.profile.name,
+								   county: this.props.profile.profile.residence.county,
+								   city: this.props.profile.profile.residence.city,
+								   state: this.props.profile.profile.residence.state});
+		}
+	}
+
+	logout() {
+		this.props.logoutUser();
+		this.props.history.push("/");
+	}
+
+	render() {
+		const { handleSubmit, error } = this.props;
+		if(this.props.profile.profile == null) {
+			return (
+					<div>Loading...</div>
+				)
+		}
+
+		if(this.state.editing == false)
+		{
+		return (
+				<div>
+				<nav className="navbar bg-primary">
+					 
+					 <Link className="navbar-brand" style={{color: '#ffffff'}} to="/">Liquidemocracy</Link>
+	                	
+	                <div className="nav-item" style={{color: '#ffffff'}} to="/profile">
+						 Profile
+						</div>
+						<Link className="nav-item active" style={{color: '#ffffff'}} to="/delegates">
+						 Delegates
+						</Link>
+						<Link className="nav-item" style={{color: '#ffffff'}} to="/delegations">
+						 Delegations
+						</Link>
+						<div className="nav-item" 
+						 	 style={{color: '#ffffff', cursor:'pointer'}} 
+						 	 onClick={() => this.logout()}>
+						 Log Out
+						</div>
+						 	
+					</nav>
+					<h1 className="border border-dark text-center">Profile</h1>
+					<div className="container-fluid">
+						<h4>Email: {this.props.profile.profile.email}</h4>
+						<h4>Name: {this.props.profile.profile.name}</h4>
+						<h4>County: {this.props.profile.profile.residence.county}</h4>
+						<h4>City: {this.props.profile.profile.residence.city}</h4>
+						<h4>State: {this.props.profile.profile.residence.state}</h4>
+
+						<button className="btn btn-warning" onClick={() => this.setState({editing: true})}>Edit</button>
+					</div>
+				</div>
+			)
+		}
+		else
+		{
+			return (
+				<div>
+				<nav className="navbar bg-primary">
+					 
+					 <Link className="navbar-brand" style={{color: '#ffffff'}} to="/">Liquidemocracy</Link>
+	                	
+	                <div className="nav-item" style={{color: '#ffffff'}} to="/profile">
+						 Profile
+						</div>
+						<Link className="nav-item active" style={{color: '#ffffff'}} to="/delegates">
+						 Delegates
+						</Link>
+						<Link className="nav-item" style={{color: '#ffffff'}} to="/delegations">
+						 Delegations
+						</Link>
+						<div className="nav-item" 
+						 	 style={{color: '#ffffff', cursor:'pointer'}} 
+						 	 onClick={() => this.logout()}>
+						 Log Out
+						</div>
+						 	
+					</nav>
+					<h1 className="border border-dark text-center">Profile</h1>
+					<div className="container-fluid">
+						<div className="profile-form" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+							<form>
+								<Field 
+									label="Email"
+									name="email"
+									type="email"
+									component={this.renderField}
+								/>
+								<Field 
+									label="Name"
+									name="name"
+									type="text"
+									component={this.renderField}
+								/>
+								<Field 
+									label="County"
+									name="county"
+									type="text"
+									component={this.renderField}
+								/>
+								<Field 
+									label="City"
+									name="city"
+									type="text"
+									component={this.renderField}
+								/>
+								<Field 
+									label="State"
+									name="state"
+									type="text"
+									component={this.renderField}
+								/>
+								<button className="btn btn-success" type="submit">Save</button>
+								<button type="button" className="btn btn-danger" onClick={() => this.setState({editing: false})}>Cancel</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			)
+		}
+	}
+
+}
+
+function mapStateToProps(state) {
+	return {
+		profile: state.profile
+	} 
+}
+
+function validate(values) {
+
+		const errors = {};
+
+		if(!values.email) {
+			errors.email = "Email blank.";
+		}
+
+		if(!values.name) {
+			errors.name = "Name blank.";
+		}
+
+		if(!values.county) {
+			errors.county = "County blank.";
+		}
+
+		if(!values.city) {
+			errors.city = "City blank.";
+		}
+
+		if(!values.state) {
+			errors.state = "State blank.";
+		}
+
+		return errors;
+	
+}
+
+export default reduxForm({
+	validate,
+	form: 'ProfileForm',
+	keepDirtyOnReinitialize: true
+})(
+connect(mapStateToProps, {logoutUser, fetchProfile})(Profile)
+);
