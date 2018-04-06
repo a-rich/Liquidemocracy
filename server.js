c67d9092e49ea3b8aef4dbcef2f19bb37f009db4
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-06 14:31:35 -0700

    more fixes

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname));
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, '0.0.0.0');
