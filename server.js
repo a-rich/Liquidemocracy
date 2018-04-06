80ace035cb05bb6e675e4476ef2ab582920599bb
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-06 16:35:45 -0700

    fix

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname));
-app.listen(process.env.PORT, '0.0.0.0', function() { console.log('listening')});
+app.listen(process.env.PORT);
