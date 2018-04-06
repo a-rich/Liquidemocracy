69e56e4b49a53849097f3bf28264e7e813f80714
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-06 14:27:14 -0700

    Small fix

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname));
-app.listen(3000, function() { console.log('listening')});
+app.listen(process.env.PORT || 5000);
