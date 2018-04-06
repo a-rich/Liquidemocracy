8310bb46ff93884b9ecf9a04ada4769c2e32a990
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-06 16:27:36 -0700

    fix

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 var express = require('express');
 var app = express();
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-
 app.use('/', express.static(__dirname));
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+app.listen(3000, function() { console.log('listening')});
