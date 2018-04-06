ff6a97e114ba75dfb34eaaf6c1666a4eb7c23363
xbrad831x <mercadobrandon33@yahoo.com>
2018-04-06 16:29:35 -0700

    fix

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname));
-app.listen(3000, function() { console.log('listening')});
+app.listen(3000, '0.0.0.0', function() { console.log('listening')});
