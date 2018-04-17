58ed15fcd15c98b089d7346835319cc620f2d236
Alex <alexrichards92@me.com>
2018-04-17 10:28:45 -0700

    testing voting

--- liquidemocracy/views/bill.py
+++ liquidemocracy/views/bill.py
@@ -115,15 +115,13 @@ def vote():
             vote_obj.vote = vote
 
         bill.save()
-        print("\nInterest vector (before): {}\n".format(str(user.interest_vector)))
+        print("\nInterest vector (before): {}\n".format(str(user.interest_vector.to_json())))
         reformatted = '_'.join([word.lower() for word in bill.category.split()])
-        print("\nBill category: {}\ncategory (reformatted): {}\n".format(
-            bill.category, reformatted))
         vector_dict = user.interest_vector.to_json()
         vector_dict[reformatted] += 3
         vector_mongo = vector_dict.to_mongo()
         user.interest_vector = vector_mongo
-        print("\nInterest vector (after): {}\n".format(str(user.interest_vector)))
+        print("\nInterest vector (after): {}\n".format(str(user.interest_vector.to_json())))
         user.save()
 
     for v in user.cast_votes:
@@ -183,15 +181,13 @@ def delegate():
     user.delegated_votes.append(delegated_vote)
     delegate.received_votes.append(delegated_vote)
 
-    print("\nInterest vector (before): {}\n".format(str(user.interest_vector)))
+    print("\nInterest vector (before): {}\n".format(str(user.interest_vector.to_json())))
     reformatted = '_'.join([word.lower() for word in bill.category.split()])
-    print("\nBill category: {}\ncategory (reformatted): {}\n".format(
-        bill.category, reformatted))
     vector_dict = user.interest_vector.to_json()
     vector_dict[reformatted] -= 1
     vector_mongo = vector_dict.to_mongo()
     user.interest_vector = vector_mongo
-    print("\nInterest vector (after): {}\n".format(str(user.interest_vector)))
+    print("\nInterest vector (after): {}\n".format(str(user.interest_vector.to_json())))
     user.save()
     delegate.save()
 
