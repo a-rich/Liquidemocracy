0c3eadcc3e010139b30b1d048e1eb1c8c3dae787
Alex Richards <alex.richards006@gmail.com>
2018-05-02 18:06:45 -0700

    test

--- liquidemocracy/bill_recommender/recommender.py
+++ liquidemocracy/bill_recommender/recommender.py
@@ -185,7 +185,7 @@ def recommend_bills(user_email, filtered_levels, index, limit, query=""):
 
     return recommended_bills
 
-classes = json.load(open(''liquidemocracy/bill_classifier/class_mapping.json', 'r'))
+classes = json.load(open('liquidemocracy/bill_classifier/class_mapping.json', 'r'))
 levels = ['federal', 'state', 'county', 'city']
 
 #potential_delegates = find_delegates(user, non_interests)
